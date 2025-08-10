ad3c40ec731ed3a2397f80ffdabd0235eb51d623
Documenter.jl <documenter@juliadocs.github.io>
2025-08-10 17:53:32 +0000

    build based on ccf47c1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Argo","page":"Home","title":"Argo","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Argo.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}

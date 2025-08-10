5e2c1f638b42553ea9810e778b590ac8b321c9d9
Sofiane <sofiane.tanji@uclouvain.be>
2025-08-10 20:23:43 +0200

    Correction of tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,11 +4,22 @@ using Aqua
 using JET
 
 @testset "Argo.jl" begin
-    @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(Argo)
-    end
-    @testset "Code linting (JET.jl)" begin
-        JET.test_package(Argo; target_defined_modules = true)
-    end
+    # @testset "Code quality (Aqua.jl)" begin
+    #     Aqua.test_all(Argo)
+    # end
+    # @testset "Code linting (JET.jl)" begin
+    #     JET.test_package(Argo; target_defined_modules=true)
+    # end
     # Write your tests here.
+    for (root, dirs, files) in walkdir(@__DIR__)
+        for file in files
+            if isnothing(match(r"^test-.*\.jl$", file))
+                continue
+            end
+            title = titlecase(replace(splitext(file[6:end])[1], "-" => " "))
+            @testset "$title" begin
+                include(file)
+            end
+        end
+    end
 end
--- test/test-language.jl
+++ test/test-language.jl
@@ -1,6 +1,7 @@
-using Revise
 using Argo
 using Argo.Language
+using Argo.Properties
+using Argo.Oracles
 using Test
 
 @variable x::R()
@@ -9,11 +10,42 @@ using Test
 expr = f(x) + g(x)
 comp = f(g(x))
 
-using Argo.Properties
 @property f Convex() Smooth(2.0)
 @property g Linear(Interval(5.0, 8.0), Interval(9.0, 15.0))
 @property g Linear(Interval(2.0, 6.0), Interval(3.0, 7.0))
 
-using Argo.Oracles
 @oracle f EvaluationOracle(x -> 2x) DerivativeOracle(x -> 2)
 @oracle g EvaluationOracle(x -> x + 1) DerivativeOracle(x -> 1)
+
+@testset "Argo Language Tests" begin
+    @testset "Expression Evaluation" begin
+        @test expr isa Language.Addition
+        @test comp isa Language.Composition
+    end
+end
+
+@testset "Argo Properties" begin
+    @test Convex() in get_properties(f)
+    @test Smooth(2.0) in get_properties(f)
+
+    g_props = get_properties(g)
+    @test length(g_props) == 1
+    g_linear_prop = first(g_props)
+    @test g_linear_prop isa Properties.Linear
+    @test g_linear_prop.eigmin == Properties.Interval(2.0, 6.0)
+    @test g_linear_prop.eigmax == Properties.Interval(3.0, 7.0)
+end
+
+@testset "Argo Oracles" begin
+    f_eval = get_oracle(f, EvaluationOracle)
+    f_deriv = get_oracle(f, DerivativeOracle)
+    @test f_eval isa Oracles.EvaluationOracle
+    @test f_deriv isa Oracles.DerivativeOracle
+    @test f_eval.f(3.0) == 6.0
+    @test f_deriv.f(3.0) == 2.0
+
+    g_eval = get_oracle(g, EvaluationOracle)
+    g_deriv = get_oracle(g, DerivativeOracle)
+    @test g_eval.f(3.0) == 4.0
+    @test g_deriv.f(3.0) == 1.0
+end
